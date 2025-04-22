8832f9e6a40ee31422b4fe9c21a0435556adcbb4
Vignesh <techvicky2018@gmail.com>
2025-04-23 01:55:31 +0530

    feat: Dynamic Messages and User Input

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -2,21 +2,41 @@ import { ChatMessage } from '@/types/message';
 import React, { useState } from 'react';
 import MessageList from './messageList/MessageList';
 import MessageInput from './messageInput/MessageInput';
+import { v4 as uuidv4 } from 'uuid';
 
 interface ChatbotProps {}
 
 const Chatbot: React.FC<ChatbotProps> = () => {
-  const [messageList, setMessageList] = useState<ChatMessage[]>([
-    { id: '1', type: 'SERVER', content: 'Initial Server message' },
-    { id: '2', type: 'CLIENT', content: 'Initial Client message' },
-  ]);
+  const [messageList, setMessageList] = useState<ChatMessage[]>([]);
 
-  console.log('current message list:::', messageList);
+  const addMessageToList = (newMessage: ChatMessage) => {
+    setMessageList((prevMessages) => [...prevMessages, newMessage]);
+  };
+
+  const handleSendMessage = (userMessage: string) => {
+    const newUserMessage: ChatMessage = {
+      id: uuidv4(),
+      type: 'CLIENT',
+      content: userMessage,
+    };
+
+    addMessageToList(newUserMessage);
+
+    setTimeout(() => {
+      const newBotMessage: ChatMessage = {
+        id: uuidv4(),
+        type: 'SERVER',
+        content: 'This is a sample response from the chatbot!',
+      };
+
+      addMessageToList(newBotMessage);
+    }, 3000);
+  };
 
   return (
-    <div className="mx-auto flex flex-col max-w-md">
+    <div className="mx-auto flex max-w-md flex-col">
       <MessageList messageList={messageList} />
-      <MessageInput />
+      <MessageInput onSendMessage={handleSendMessage} />
     </div>
   );
 };
--- src/components/chatbot/messageInput/MessageInput.tsx
+++ src/components/chatbot/messageInput/MessageInput.tsx
@@ -3,23 +3,30 @@ import { Input } from '@/components/ui/input';
 import React, { useState } from 'react';
 import { TbSend2 } from 'react-icons/tb';
 
-const MessageInput = () => {
+interface MessageInputProps {
+  onSendMessage: (content: string) => void;
+}
+
+const MessageInput: React.FC<MessageInputProps> = (props) => {
+  const { onSendMessage } = props;
+
   const [messageInput, setMessageInput] = useState<string>('');
 
-  const handleSendMessage = () => {
+  const sendMessage = () => {
     console.log('Input message:::', messageInput);
+    onSendMessage(messageInput);
     setMessageInput('');
   };
 
   return (
-    <div className="flex">
+    <div className="flex gap-1">
       <Input
         type="text"
         placeholder="Ask me something..."
         value={messageInput}
         onChange={(e) => setMessageInput(e.target.value)}
       />
-      <Button size="icon" onClick={handleSendMessage} className="rounded-full border">
+      <Button size="icon" onClick={sendMessage} className="rounded-full border">
         <TbSend2 />
       </Button>
     </div>
